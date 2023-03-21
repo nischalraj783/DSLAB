d097e3160ce443cf57fc3f10a67b35a48eb5053c
nischalraj783 <99309586+nischalraj783@users.noreply.github.com>
2023-03-21 19:53:36 +0530

    Update CQusingArray.c

--- CQusingArray.c
+++ CQusingArray.c
@@ -1,101 +1,81 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
+#define size 5
+int arr[size];
+int R=-1,F=0,te=0;
 
-#define MAX_SIZE 5
-
-struct CircularQueue {
-    int front, rear;
-    int items[MAX_SIZE];
-};
-
-struct CircularQueue* createCircularQueue() {
-    struct CircularQueue* newQueue = (struct CircularQueue*)malloc(sizeof(struct CircularQueue));
-    newQueue->front = -1;
-    newQueue->rear = -1;
-    return newQueue;
-}
-
-int isEmpty(struct CircularQueue* queue) {
-    return (queue->front == -1 && queue->rear == -1);
-}
-
-int isFull(struct CircularQueue* queue) {
-    return ((queue->rear+1) % MAX_SIZE == queue->front);
-}
-
-void enqueue(struct CircularQueue* queue, int data) {
-    if (isFull(queue)) {
-        printf("Circular queue is full.\n");
-    } else if (isEmpty(queue)) {
-        queue->front = queue->rear = 0;
-        queue->items[queue->rear] = data;
-    } else {
-        queue->rear = (queue->rear+1) % MAX_SIZE;
-        queue->items[queue->rear] = data;
+void push()
+{
+    int n;
+    if(te==size)
+    {
+      printf("Queue is full");
+    }
+    else
+    {
+        printf("Enter a number ");
+        scanf("%d",&n);
+        R=(R+1)%size;
+        arr[R]=n;
+        te=te+1;             
     }
 }
-
-int dequeue(struct CircularQueue* queue) {
-    int deletedData = -1;
-    if (isEmpty(queue)) {
-        printf("Circular queue is empty.\n");
-    } else if (queue->front == queue->rear) {
-        deletedData = queue->items[queue->front];
-        queue->front = queue->rear = -1;
-    } else {
-        deletedData = queue->items[queue->front];
-        queue->front = (queue->front+1) % MAX_SIZE;
+void pop()
+{
+    if(te==0)
+    {
+        printf("Queue is empty");
     }
-    return deletedData;
+    else
+    {
+        printf("Number Deleted = %d",arr[F]);
+        F=(F+1)%size;
+        te=te-1;
+     }
 }
-
-void display(struct CircularQueue* queue) {
-    if (isEmpty(queue)) {
-        printf("Circular queue is empty.\n");
-    } else {
-        printf("Elements in the circular queue are: ");
-        int i;
-        for (i = queue->front; i != queue->rear; i = (i+1) % MAX_SIZE) {
-            printf("%d ", queue->items[i]);
-        }
-        printf("%d\n", queue->items[i]);
-    }
+void display()
+{
+     int i,x;
+     if(te==0)
+     {
+        printf("Queue is empty");
+     }
+     else
+     {    
+         x=F;
+         for(i=1; i<=te; i++)
+         {
+            printf("%d ",arr[x]);
+            x=(x+1)%size;
+         }
+      }
 }
-
-int main() {
-    struct CircularQueue* queue = createCircularQueue();
-    int choice, data;
-
-    while (1) {
-        printf("\nCircular Queue Menu:\n");
-        printf("1. Enqueue\n");
-        printf("2. Dequeue\n");
+int main()
+{
+    int choice;
+   while(choice!=4)
+   {
+        printf("1. Add\n");
+        printf("2. Delete\n");
         printf("3. Display\n");
         printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter data to enqueue: ");
-                scanf("%d", &data);
-                enqueue(queue, data);
+        printf("Enter Choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:push();
                 break;
-            case 2:
-                data = dequeue(queue);
-                if (data != -1) {
-                    printf("Deleted element: %d\n", data);
-                }
+            case 2:pop();
                 break;
-            case 3:
-                display(queue);
+                
+            case 3:display();
                 break;
             case 4:
-                exit(0);
+                 break;
             default:
-                printf("Invalid choice.\n");
+                printf("Wrong Choice");
         }
-    }
-
-    return 0;
+   }
+   return;
 }
